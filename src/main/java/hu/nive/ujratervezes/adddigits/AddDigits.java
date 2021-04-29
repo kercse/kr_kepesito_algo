264a2ef27dea1ef7f7a6356fee4ab01ddc54a4a2
kercse <69111507+kercse@users.noreply.github.com>
2021-04-29 11:24:42 +0200

    update_on_struggling

--- src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
+++ src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
@@ -1,5 +1,28 @@
 package hu.nive.ujratervezes.adddigits;
 
 public class AddDigits {
+    public int addDigits(String input) {
+
+        int result = -1;
+
+        if (input != null) {
+
+            input = input.replaceAll("[^0-9]", "");
+            input.trim();
+
+            if (input.length() > 0) {
+                result = 0;
+                char[] charArray;
+                charArray = input.toCharArray();
+                for (int i = 0; i < charArray.length; i++){
+                    result = result + charArray[i];
+                }
+            }
+        }
+        return result;
+
+    }
+
+
     //todo: implement your solution here
 }
